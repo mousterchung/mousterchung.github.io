337d6938ee3908c4ee835daa66b0043c13a49e2d
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2025-03-09 05:26:29 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,7 @@
 function get_scrollbar_width() {
-	const userAgent = navigator.userAgent;
-	
-	if (userAgent.indexOf("Win") > -1) {
-		if ((userAgent.indexOf("Chrome") > -1) || (userAgent.indexOf("Firefox") > -1)) {
-			return 17;
-		} else if (userAgent.indexOf("Edge") > -1) {
-			return 12;
-		}
-	} else if (userAgent.indexOf("Mac") > -1) {
-		return 15; 
-	}
-	return 17; 
+	return window.innerWidth - document.documentElement.clientWidth; 
 }
 
 scrollbar_width = get_scrollbar_width(); 
-console.log(scrollbar_width); 
-document.documentElement.style.setProperty('scrollbar-width', scrollbar_width); 
+console.log("Scroll Bar Width: ", scrollbar_width); 
+document.documentElement.style.setProperty('--scrollbar-width', scrollbar_width); 
